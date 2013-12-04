ca22620f679545e42dc79e2126f26e9d9e71a76c
Jeremy Mikkola <jmikkola@thumbtack.com>
2013-12-03 17:40:16 -0800

    Half of the javascript side

--- public/tasks.js
+++ public/tasks.js
@@ -1,5 +1,27 @@
 (function() {
-    "use strict";
+    'use strict';
 
-    var tasks = angular.app('tasks', []);
+    var tasks = angular.module('tasks', []);
+
+    tasks.controller('TasksCtrl', [
+        '$scope',
+        function($scope) {
+            $scope.title = '';
+            $scope.tasks = [];
+
+            function saveTask(task) {
+                $scope.tasks.push({
+                    task: task,
+                    time: new Date()
+                });
+            }
+
+            $scope.addTask = function() {
+                if ($scope.title) {
+                    saveTask($scope.title);
+                    $scope.title = '';
+                }
+            };
+        }
+    ]);
 }());
