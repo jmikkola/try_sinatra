a31068ba2836f62a03f4631c6dc8c9dc0a8e4974
Jeremy Mikkola <jmikkola@thumbtack.com>
2013-12-04 16:24:30 -0800

    Mostly complete transition to views

--- hello.rb
+++ hello.rb
@@ -25,12 +25,6 @@ get '/hello/:name' do |name|
     haml :hello, :locals => {:name => name}
 end
 
-def render_nav(solutions)
-    haml :euler_nav, :locals => {
-        :solved => solutions.keys,
-    }
-end
-
 get %r{/euler/(\d+)} do |problem|
     template, values = EulerView.new.render(problem)
     haml template, :locals => values
@@ -67,18 +61,6 @@ post '/tasks/complete' do
 end
 
 get '/tasks/tag/:tag_name' do |tag_name|
-    tasks = Task.all(
-        :done_time => nil,
-        :task_tag => {tag_tag: tag_name},
-        :order => [ :create_time.asc ]
-    )
-    tasks_json = (tasks.map { |task| task.to_hash }).to_json
-
-    completed_count = Task.count(:done_time.not => nil, :task_tag => {:tag_tag => tag_name})
-
-    haml :tasks_tag, :locals => {
-        :tag_name => tag_name,
-        :tasks => tasks_json,
-        :completed => completed_count,
-    }
+    template, values = TaskTagView.new(tag_name).render
+    haml template, :locals => values
 end
--- views/tasks.rb
+++ views/tasks.rb
@@ -19,3 +19,30 @@ class TasksView
         Task.count(:done_time.not => nil)
     end
 end
+
+class TaskTagView
+    def initialize(tag_name)
+        @tag_name = tag_name
+    end
+
+    def render
+        return :tasks_tag, values
+    end
+
+    def values
+        { :tasks => get_tasks.to_json, :completed => count_completed, :tag_name => @tag_name }
+    end
+
+    def get_tasks
+        tasks = Task.all(
+            :done_time => nil,
+            :task_tag => {:tag_tag => @tag_name},
+            :order => [ :create_time.asc ],
+        )
+        return tasks.map { |task| task.to_hash }
+    end
+
+    def count_completed
+        Task.count(:done_time.not => nil, :task_tag => {:tag_tag => @tag_name})
+    end
+end
